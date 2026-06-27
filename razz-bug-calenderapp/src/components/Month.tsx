94b455eabe82e41bf044e64c1b37a755ceb3e72e
TripleAze <aliyuatikuabubakar7@gmail.com>
2026-06-27 09:45:56 +0100

    Testing dashboard

--- razz-bug-calenderapp/src/components/Month.tsx
+++ razz-bug-calenderapp/src/components/Month.tsx
@@ -6,7 +6,7 @@ interface MonthProps {
   highlightToday: boolean;
 }
 
-const Weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const Weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const Month: React.FC<MonthProps> = ({ date, highlightToday }) => {
   const year = date.getFullYear();
